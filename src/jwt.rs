7f16e0738230389b39894afc3e98054ad667c5cc
SchwabJust1n <github.depict091@passmail.net>
2025-01-19 20:48:37 +0100

    fixed

--- src/jwt.rs
+++ src/jwt.rs
@@ -1,13 +1,13 @@
-use chrono::{DateTime, Duration, Utc};
+use chrono::{Duration, Utc};
 use cookie::{CookieBuilder, time::OffsetDateTime};
 use jsonwebtoken::{DecodingKey, EncodingKey, Header, Validation, decode, encode};
 use rocket::http::Cookie;
 use serde::{Deserialize, Serialize};
 use std::{borrow::Cow, convert::TryFrom, env};
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct Jwt<T> {
-    pub exp: Option<DateTime<Utc>>,
+    pub exp: i64,
     pub claims: T,
 }
 
@@ -16,41 +16,33 @@ where
     T: Serialize + for<'de> Deserialize<'de>,
 {
     pub fn new(claims: T) -> Self {
-        Self { exp: None, claims }
+        Self { exp: 0, claims }
     }
 
     pub fn new_with_exp(claims: T, duration: Duration) -> Self {
-        Self {
-            exp: Some(Utc::now() + duration),
-            claims,
-        }
+        let exp = (Utc::now() + duration).timestamp();
+        Self { exp, claims }
     }
 
     pub fn is_expired(&self) -> bool {
-        match self.exp {
-            Some(exp) => Utc::now() > exp,
-            None => false,
-        }
+        self.exp != 0 && Utc::now().timestamp() > self.exp
     }
 
     pub fn validate(token: &str) -> anyhow::Result<Self> {
         let secret = env::var("JWT_SECRET")?;
-        let data = decode::<T>(
+        let data = decode::<Self>(
             token,
             &DecodingKey::from_secret(secret.as_bytes()),
             &Validation::default(),
         )?;
-        Ok(Self {
-            exp: None,
-            claims: data.claims,
-        })
+        Ok(data.claims)
     }
 
     pub fn sign(&self) -> anyhow::Result<String> {
         let secret = env::var("JWT_SECRET")?;
         Ok(encode(
             &Header::default(),
-            &self.claims,
+            &self,
             &EncodingKey::from_secret(secret.as_bytes()),
         )?)
     }
@@ -60,13 +52,12 @@ where
         name: impl Into<Cow<'static, str>>,
         client_exp: bool,
     ) -> anyhow::Result<CookieBuilder<'c>> {
-        let builder = CookieBuilder::new(name, self.sign()?);
-        match self.exp {
-            Some(exp) if client_exp => {
-                Ok(builder.expires(OffsetDateTime::from_unix_timestamp(exp.timestamp())?))
-            }
-            _ => Ok(builder),
+        let mut builder = CookieBuilder::new(name, self.sign()?);
+
+        if client_exp && self.exp != 0 {
+            builder = builder.expires(OffsetDateTime::from_unix_timestamp(self.exp)?);
         }
+        Ok(builder)
     }
 }
 
@@ -76,6 +67,6 @@ where
 {
     type Error = anyhow::Error;
     fn try_from(value: Cookie<'_>) -> Result<Self, Self::Error> {
-        Self::validate(value.value_trimmed())
+        Self::validate(value.value())
     }
 }
